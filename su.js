43c0287946e81f9c189145c8828140c6691a88cf
Aaron Gray <aaron@hellometa.com>
2014-10-18 02:59:30 -0600

    Define entery point for Node and include starting su.js file.

--- su.js
+++ su.js
@@ -1,3 +1,10 @@
 var sass = require('node-sass');
 
+sass.render({
+  file: 'templates/_grids.scss',
+  success: function(css) {
+    console.log(css);
+  }
+});
 
+// susy-count(1 2 3 4 5)1 2 3 4 6
